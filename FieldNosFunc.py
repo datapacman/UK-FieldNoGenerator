7e39fe38a48c8a2a9be55790aedd0d8445878e9c
datapacman <datapacman@users.noreply.github.com>
2014-11-01 11:57:06 +0000

    Fixed issues, works better

--- FieldNosFunc.py
+++ FieldNosFunc.py
@@ -1,16 +1,24 @@
 # Author: KU
 # Date: 28 March 2014
+# This Revision: 17 July 2014
 # Version: ArcGIS 10.1
-# Prerequisites: Columns of "X" and "Y" co-ordinates.
-# Purpose: This script will add a field named "FieldNos" to a given Feature Class and populate with the Field No, generated from the X and Y fields.
-# Known issue: Feature Class won't display newly created data in attribute table without removing and re-adding to the mxd.
+# Purpose: This script will add fields named "FieldNos", "X" and "Y" to a given Feature Class and populate with the Field No, generated from the X and Y fields.
+
  
  
 import os, string, arcpy
  
 arcpy.env.workspace = arcpy.GetParameterAsText(0)
 featureClass = arcpy.GetParameterAsText(1)
+ 
 arcpy.AddField_management(featureClass, "FieldNos", "text")
+arcpy.AddField_management(featureClass, "X", "LONG")
+arcpy.AddField_management(featureClass, "Y", "LONG")
+fields = ("FieldNos", "X", "Y")
+ 
+arcpy.CalculateField_management(featureClass, "X", "!SHAPE.CENTROID.X!", "PYTHON_9.3")
+arcpy.CalculateField_management(featureClass, "Y", "!SHAPE.CENTROID.Y!", "PYTHON_9.3")
+ 
  
 def calculation(axis):
     b = str(axis)
@@ -21,13 +29,13 @@ def calculation(axis):
         return result
     return result
  
-rows = arcpy.UpdateCursor(featureClass)
+with arcpy.da.UpdateCursor(featureClass, fields) as rows:
  
-for row in rows:
-    resultx = calculation(row.X)
-    resulty = calculation(row.Y)
-    row.FieldNos = str(resultx).zfill(2) + str(resulty).zfill(2)
-    rows.updateRow(row)
+    for row in rows:
+        resultx = calculation(row[1])
+        resulty = calculation(row[2])
+        row[0] = str(resultx).zfill(2) + str(resulty).zfill(2)
+        rows.updateRow(row)
  
-del row
-del rows
+#del row
+#del rows
